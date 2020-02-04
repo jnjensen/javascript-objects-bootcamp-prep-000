301e3c631720c3dfa63e10454773d21ffb569228
James Jensen <jnjensen@live.com>
2020-02-04 03:12:17 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -2,3 +2,8 @@ var playlist = {
   voodooBlue: "Too Old to Cry",
   cher: "Believe"
 }
+
+function updatePlaylist(playlist, artistName, songTitle){
+  playlist.artistName = songTitle
+  return playlist
+}
